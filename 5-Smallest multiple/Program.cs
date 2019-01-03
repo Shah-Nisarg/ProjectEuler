ffffc1c2f619f887e99255e22d47329306e11864
Nisarg Shah <Shah-Nisarg@users.noreply.github.com>
2019-01-03 22:04:40 +0530

    Solved 5

---
+++ 5-Smallest multiple/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _5_Smallest_multiple
+{
+    class Program
+    {
+        const int N = 20;
+
+        static void Main(string[] args)
+        {
+            int number = 0;
+            while(true)
+            {
+                number++;
+                if (IsNumberDivisibleBy1ToN(number))
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine($"{number} is divisible by all numbers between 1 to {N}.");
+        }
+
+        static bool IsNumberDivisibleBy1ToN(int number)
+        {
+            for (int i = N; i > 1; i--)
+            {
+                if (number % i != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
