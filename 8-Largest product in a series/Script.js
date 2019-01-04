9ff3ece59c1995b5215220386eb8d20143d16471
Nisarg Shah <Shah-Nisarg@users.noreply.github.com>
2019-01-04 23:43:28 +0530

    Solved 8
    Seemed simple enough for JS

---
+++ 8-Largest product in a series/Script.js
@@ -0,0 +1,64 @@
+
+var inputNumber = `73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450`.replace(/\s/g,'');
+
+// Strings that have a zero will result in multiplication of zero. So splitting by them is okay.
+var nonZeroStrings = inputNumber.split("0");
+var nonZeroStringsLongerThan13Digits = nonZeroStrings.filter(t => t.length >= 13);
+// console.log(nonZeroStringsLongerThan13Digits);
+
+// Need to get all possible 13 digit strings.
+function take13Digits (number) {
+  return /\d{13}/g.exec(number)[0];
+}
+
+var allPossibleNumbers = [];
+nonZeroStringsLongerThan13Digits.forEach(t => {
+  var digits = t.split("");
+  while(digits.length >= 13) {
+    allPossibleNumbers.push(take13Digits(digits.join("")));
+    digits.shift();
+  }
+});
+
+console.log(allPossibleNumbers);
+
+
+// Get the answer.
+function getMultiplication(number) {
+  var digits = number.split("");
+  var mult = 1;
+  digits.forEach(t => mult *= t);
+  return mult;
+}
+
+var largestMultiplication = 0;
+var numberWithLargestMultiplication = 0;
+allPossibleNumbers.forEach(t => {
+  var mult = getMultiplication(t);
+  if (mult > largestMultiplication) {
+    largestMultiplication = mult;
+    numberWithLargestMultiplication = t;
+  }
+});
+
+console.log("Winning number:", numberWithLargestMultiplication);
+console.log("Answer: ", largestMultiplication);
